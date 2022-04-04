cb1157e8ff54a0659ae33934ab5e1c1d05531397
Bruno Vitte <bruno_vitte@hotmail.com>
2022-04-04 16:15:59 -0500

    aun no acaba

--- figuras.js
+++ figuras.js
@@ -1,1 +1,37 @@
-console.log("Hello, world!")
+// Código del cuadrado
+console.group("Cuadrados")
+
+const ladoCuadrado = 5
+
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm")
+
+const perimetroCuadrado = ladoCuadrado * 4
+
+console.log("El perimetro del cuadrado:" + perimetroCuadrado + "cm")
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado
+
+console.log("El area del cuadrodo es: " + areaCuadrado + "cm")
+
+console.groupEnd();
+
+// Código del triángulo
+console.group("Triangulos")
+
+const ladoTriangulo1 = 6
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+console.log(
+    "Los lados del triángulo miden: "
+    + ladoTriangulo1
+	+ "cm, "
+	+ ladoTriangulo2
+	+ "cm, "
+	+ baseTriangulo
+	+ "cm"
+);
+
+console.groupEnd();
+
